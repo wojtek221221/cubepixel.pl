4cf38af877a3bdf22ea94ca01c1996d07039755a
wojtus <131788501+wojtek221221@users.noreply.github.com>
2025-01-16 23:11:54 +0100

    Add files via upload

---
+++ Landing-page/src/js/scroll.js
@@ -0,0 +1,41 @@
+document.getElementById('scroll').addEventListener('click', function () {
+    const target = document.getElementById('tryby');
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+        setTimeout(() => {
+            target.classList.add('opacity-100');
+        }, 500);
+    }
+});
+document.getElementById('zarzad_link').addEventListener('click', function () {
+    const target = document.getElementById('zarzad');
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+        setTimeout(() => {
+            target.classList.add('opacity-100');
+        }, 500);
+    }
+});
+document.getElementById('zarzad_link2').addEventListener('click', function () {
+    const target = document.getElementById('zarzad');
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+        setTimeout(() => {
+            target.classList.add('opacity-100');
+        }, 500);
+    }
+});
+document.getElementById('main_link').addEventListener('click', function () {
+    const target = document.getElementById('main');
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+        setTimeout(() => {
+            target.classList.add('opacity-100');
+        }, 500);
+    }
+});
+
